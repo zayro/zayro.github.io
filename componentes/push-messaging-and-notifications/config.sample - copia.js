5bacfcbf2bee180dd5537476ae87a0155ef7739a
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2017-02-24 16:40:56 -0500

    ajustes de rutas y complementos

--- componentes/push-messaging-and-notifications/config.sample - copia.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Public API Key from https://console.developers.google.com>'
-};
